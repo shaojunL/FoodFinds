043aa8d4c5c6dc768362a576e76acc4d1293e5ac
charmainelow1 <125853983+charmainelow1@users.noreply.github.com>
2024-02-05 18:39:44 +0800

    Revert "Merge branch 'main' of https://github.com/shaojunL/Techfest-Hackerthon-2024"
    This reverts commit e21e8f4951a4646aea98268bfcf500bfa70ada3d, reversing
    changes made to 8b45e4c250f63dcf694834f5a9eee38444ade001.

--- test.js
+++ test.js
@@ -1,20 +1,6 @@
 const {recipe1} = require('./generate');
 foods = 'broccoli, chicken, fish, vegetables, cabbage, eggs'
-
-async function main(foods) {
-    try {
-      const res = await recipe1(foods);
-      console.log(res);
-    } catch (error) {
-      console.error("Error in main function response:", error);
-    }
-  }
-  
-main(foods);
-  
-
-
-
+recipe1(foods)
 
 
     
