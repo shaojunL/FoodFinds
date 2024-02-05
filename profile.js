c22263083db1a7e38e2d44206553093f15f4a09c
charmainelow1 <125853983+charmainelow1@users.noreply.github.com>
2024-02-05 18:51:18 +0800

    Revert to correct profile :)

--- profile.js
+++ profile.js
@@ -5,11 +5,12 @@ import { getDatabase, ref, push, onValue, remove, update, set} from "https://www
 
 const database = getDatabase(app); 
 const signOutButton = document.getElementById("signOutButton"); 
-const emailAddressEl = document.getElementById("emailAddress"); 
-const dietaryRestrictionInputEl = document.getElementById("dietaryRestrictionInput"); 
-const cuisinePreferenceInputEl = document.getElementById("cuisinePreferenceInput"); 
+const helloText = document.getElementById("helloText"); 
+const inputOneEl = document.getElementById("input1"); 
+const inputTwoEl = document.getElementById("input2");
 const saveButtonEl = document.getElementById("saveButton"); 
-const editInfoButtonEl = document.getElementById("editInfo"); 
+const showSavedDataButtonEl = document.getElementById("showInfo"); 
+const showSavedDataTextEl = document.getElementById("savedInfo"); 
 
 const monitorAuthState = async () => {
     onAuthStateChanged(auth, user => {
@@ -26,68 +27,35 @@ const monitorAuthState = async () => {
 monitorAuthState(); 
 
 function renderPage(user) {
-    emailAddressEl.innerHTML = user.email;
+    helloText.innerHTML = "hello " + user.uid;
     saveButtonEl.addEventListener("click", function() {
         saveData(user.uid); 
     }); 
-    showSavedData(user.uid); 
+    showSavedDataButtonEl.addEventListener("click", function() {
+        showSavedData(user.uid); 
+    })
 }
 
 function saveData(uid) { 
     let userInDB = ref(database, `users/${uid}`); 
     update(userInDB, {
-        input1: dietaryRestrictionInputEl.value, 
-        input2: cuisinePreferenceInputEl.value 
+        input1: inputOneEl.value, 
+        input2: inputTwoEl.value 
     }); 
-
-    onValue(userInDB, function(snapshot){
-        if (snapshot.exists()) {
-            let dataArr = Object.entries(snapshot.val()); 
-            replaceInputWithText(dataArr); 
-        }
-    }); 
-
-    saveButtonEl.style.visibility = "hidden"; 
-}
-
-function editData(dataArr, uid, dietaryRestrictionTextEl, cuisinePreferenceTextEl){
-    replaceTextwithInput(dietaryRestrictionTextEl, cuisinePreferenceTextEl); 
-    editInfoButtonEl.style.visibility = "hidden"; 
-    saveButtonEl.addEventListener("click", ()=>{
-        showSavedData(uid); 
-        saveData(uid); 
-    }); 
-    saveButtonEl.style.visibility = "visible"; 
-}
-
-function replaceTextwithInput(dietaryRestrictionTextEl, cuisinePreferenceTextEl) {
-    dietaryRestrictionTextEl.replaceWith(dietaryRestrictionInputEl); 
-    cuisinePreferenceTextEl.replaceWith(cuisinePreferenceInputEl); 
-}
-
-function replaceInputWithText(dataArr, uid) {
-    let dietaryRestrictionTextEl = document.createElement("h3"); 
-    let cuisinePreferenceTextEl = document.createElement("h3"); 
-    dietaryRestrictionInputEl.replaceWith(dietaryRestrictionTextEl); 
-    cuisinePreferenceInputEl.replaceWith(cuisinePreferenceTextEl); 
-    dietaryRestrictionTextEl.innerText = dataArr[0][1]; 
-    cuisinePreferenceTextEl.innerText = dataArr[1][1]; 
-
-    editInfoButtonEl.addEventListener("click", () => {
-        editData(dataArr, uid, dietaryRestrictionTextEl, cuisinePreferenceTextEl); 
-    });
 }
 
 function showSavedData(uid) {
-    let userInDB = ref(database, `users/${uid}`); 
+    let userInDB = ref(database, `users/${uid}`);  
+
     onValue(userInDB, function(snapshot){
+        showSavedDataTextEl.innerHTML = ""; 
+
         if (snapshot.exists()) {
             let dataArr = Object.entries(snapshot.val()); 
-            replaceInputWithText(dataArr, uid); 
+            console.log(dataArr); 
+            showSavedDataTextEl.innerHTML = `Input 1: ${dataArr[0][1]}, Input 2: ${dataArr[1][1]}`; 
         }
     }); 
-    saveButtonEl.style.visibility = "hidden"; 
-    editInfoButtonEl.style.visibility = "visible"; 
 }
 
 async function logout() {
